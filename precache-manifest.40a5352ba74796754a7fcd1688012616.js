4b22f80c33d18279a8a2de4dc076f5f95785de7f
Bhavesh-Singh-Mertiya <26bhaveshsingh@gmail.com>
2020-09-24 17:53:57 +0530

    Updates

---
+++ precache-manifest.40a5352ba74796754a7fcd1688012616.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "766ae8c1eb7d0a17f4d3",
+    "url": "/monsters-rolodex/static/css/main.5cf1cac0.chunk.css"
+  },
+  {
+    "revision": "766ae8c1eb7d0a17f4d3",
+    "url": "/monsters-rolodex/static/js/main.f5c93437.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  },
+  {
+    "revision": "27567163bda5e8f4c8e3",
+    "url": "/monsters-rolodex/static/js/2.6a720388.chunk.js"
+  },
+  {
+    "revision": "bd5df3d4015f8c307c4c990c7aea7ad3",
+    "url": "/monsters-rolodex/index.html"
+  }
+];
